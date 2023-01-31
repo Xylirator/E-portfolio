66dcc4f98af7e145e40d5b78f4026e9cac6fddcd
umar khan <umar.khan@student.uts.edu.au>
2023-01-31 16:03:23 +1100

    added hover and float effect

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function moveBackground(event) {
     const boolInt = isOdd ? -1 : 1;
     shapes[i].style.transform = `translate(${x * boolInt}px, ${
       y * boolInt
-    }px) rotate(${x * boolInt * 10}deg)`
+    }px) rotate(${x * boolInt * 10}deg)`;
   }
 }
 
@@ -58,3 +58,40 @@ function toggleModal() {
   isModalOpen = true;
   document.body.classList += " modal--open";
 }
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const enhance = (id) => {
+  const element = document.getElementById(id),
+    text = element.innerText.split("");
+
+  element.innerText = "";
+
+  text.forEach((value, index) => {
+    const outer = document.createElement("span");
+
+    outer.className = "outer";
+
+    const inner = document.createElement("span");
+
+    inner.className = "inner";
+
+    inner.style.animationDelay = `${rand(-5000, 0)}ms`;
+
+    const letter = document.createElement("span");
+
+    letter.className = "letter";
+
+    letter.innerText = value;
+
+    letter.style.animationDelay = `${index * 1000}ms`;
+
+    inner.appendChild(letter);
+
+    outer.appendChild(inner);
+
+    element.appendChild(outer);
+  });
+};
+
+enhance("about__me");
